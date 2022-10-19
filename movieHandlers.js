5f4a9ce23e9e53f633ad22806ab1873594391518
ngroult <nolwenn.groult@gmail.com>
2022-10-19 17:19:20 +0200

    Remove comments

--- movieHandlers.js
+++ movieHandlers.js
@@ -1,32 +1,5 @@
 const database = require("./database");
 
-// const movies = [
-//   {
-//     id: 1,
-//     title: "Citizen Kane",
-//     director: "Orson Wells",
-//     year: "1941",
-//     colors: false,
-//     duration: 120,
-//   },
-//   {
-//     id: 2,
-//     title: "The Godfather",
-//     director: "Francis Ford Coppola",
-//     year: "1972",
-//     colors: true,
-//     duration: 180,
-//   },
-//   {
-//     id: 3,
-//     title: "Pulp Fiction",
-//     director: "Quentin Tarantino",
-//     year: "1994",
-//     color: true,
-//     duration: 180,
-//   },
-// ];
-
 const getMovies = (req, res) => {
   database
     .query("select * from movies")
