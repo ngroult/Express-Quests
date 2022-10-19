b0d495c5ce23fd9e36a6877ea1b02826e635ca73
ngroult <nolwenn.groult@gmail.com>
2022-10-19 13:49:38 +0200

    quete 3 POST

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 
 const app = express();
+app.use(express.json());
 
 const port = process.env.APP_PORT ?? 5000;
 
@@ -17,7 +18,8 @@ app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 app.get("/api/users", movieHandlers.getUsers);
 app.get("/api/users/:id", movieHandlers.getUserById);
-
+app.post("/api/movies", movieHandlers.postMovie);
+app.post("/api/users", movieHandlers.postUser);
 app.listen(port, (err) => {
   if (err) {
     console.error("Something bad happened");
--- movieHandlers.js
+++ movieHandlers.js
@@ -83,9 +83,43 @@ const getUserById = (req, res) => {
     });
 };
 
+const postMovie = (req, res) => {
+  const { title, director, year, color, duration } = req.body;
+  // console.log(req.body);
+  database
+    .query(
+      "INSERT INTO movies (title, director, year, color, duration) VALUEs (?,?,?,?,?)",
+      [title, director, year, color, duration]
+    )
+    .then(([result]) => {
+      // console.log(result.insertId);
+      res.location(`/api/movies/${result.instertId}`).sendStatus(201);
+    })
+    .catch(() => {
+      res.status(500).send("Error saving the movie");
+    });
+};
+
+const postUser = (req, res) => {
+  const { firstname, lastname, email, city, language } = req.body;
+  database
+    .query(
+      "INSERT INTO users (firstname, lastname, email, city, language) VALUES (?,?,?,?,?)",
+      [firstname, lastname, email, city, language]
+    )
+    .then(([result]) => {
+      res.location(`/api/users/${result.insertId}`).sendStatus(201);
+    })
+    .catch(() => {
+      res.status(500).send("Error saving the user");
+    });
+};
+
 module.exports = {
   getMovies,
   getMovieById,
   getUsers,
   getUserById,
+  postMovie,
+  postUser,
 };
